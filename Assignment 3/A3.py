da46ab04521a0ce0e489240a2fab2342336ee05a
ashley-fong <ashley.fong43@gmail.com>
2024-03-17 12:43:15 -0400

    pushing

---
+++ Assignment 3/A3.py
@@ -0,0 +1,148 @@
+import psycopg2
+
+def getAllStudents(connection, cursor):
+    cursor.execute("SELECT  * from students")
+    getAllStudents = cursor.fetchall()
+
+    print("getAllStudents: ")
+
+    print("ID   First Name   Last Name     Email                        Enrollment Date")
+    for i in getAllStudents:
+        formatDate = i[4].strftime('%Y-%m-%d')
+
+        print(f"{i[0]:<6}{i[1]:<13}{i[2]:<13}{i[3]:<30}{formatDate}")
+
+
+
+def addStudent(connection, cursor):
+    first_name = input("Enter the first name: ")
+    last_name = input("Enter the last name: ")
+    email = input("Enter the email: ")
+    enrollment_date = input("Enter the enrollment date [yyyy-mm-dd]: ")
+    cursor.execute("""INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+                (%s, %s, %s, %s);""",(first_name, last_name, email, enrollment_date))
+
+    connection.commit()
+
+
+def updateStudentEmail(connection, cursor):
+    email = input("Enter the new email: ")
+    student_id = input("Enter the student id: ")
+    cursor.execute("""UPDATE students SET email = %s WHERE student_id = %s;""", (email, student_id))
+
+    connection.commit()
+
+
+def deleteStudent(connection, cursor):
+    student_id = input("Enter the student id: ")
+    cursor.execute("""DELETE from students where student_id = %s;""", (student_id))
+    connection.commit()
+
+
+connection = psycopg2.connect(user="postgres",
+                                password="postgres",
+                                host="127.0.0.1",
+                                port="5432",
+                                database="assignment3")
+
+cursor = connection.cursor()
+create_table_query = '''CREATE TABLE IF NOT EXISTS students 
+        (student_id   SERIAL  PRIMARY KEY,
+        first_name    TEXT    NOT NULL,
+        last_name     TEXT    NOT NULL,
+        email         TEXT    NOT NULL    UNIQUE,
+        enrollment_date  DATE); '''
+
+cursor.execute(create_table_query)
+connection.commit()
+print("Table created successfully in PostgreSQL ")
+
+insert_query = """INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+                ('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
+                ('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
+                ('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02');"""
+
+cursor.execute(insert_query)
+connection.commit()
+print("Table inserted successfully in PostgreSQL ")
+
+while True:
+
+    print("\nMenu:")
+    print("1. getAllStudents")
+    print("2. addStudent")
+    print("3. updateStudentEmail")
+    print("4. deleteStudent")
+    print("5. exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == '1':
+        getAllStudents(connection,cursor)
+    elif choice == '2':
+        addStudent(connection,cursor)
+    elif choice == '3':
+        updateStudentEmail(connection,cursor)
+    elif choice == '4':
+        deleteStudent(connection,cursor)
+    elif choice == '5':
+        print("Exiting program.")
+        break
+    else:
+        print("Invalid choice. Please enter a valid option.")
+
+cursor.close()
+connection.close()
+print("PostgreSQL connection is closed")
+
+# create_table_query = '''CREATE TABLE students
+#         (student_id   SERIAL  PRIMARY KEY,
+#         first_name    TEXT    NOT NULL,
+#         last_name     TEXT    NOT NULL,
+#         email         TEXT    NOT NULL    UNIQUE,
+#         enrollment_date  DATE); '''
+
+# cursor.execute(create_table_query)
+# connection.commit()
+# print("Table created successfully in PostgreSQL ")
+
+# insert_query = """INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+#                 ('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
+#                 ('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
+#                 ('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02');"""
+
+# cursor.execute(insert_query)
+# connection.commit()
+# print("Table inserted successfully in PostgreSQL ")
+
+# cursor.execute("SELECT  * from students")
+# getAllStudents = cursor.fetchall()
+
+# print("\n")
+
+# print("getAllStudents: ")
+# for i in getAllStudents:
+#     formatDate = i[4].strftime('%Y-%m-%d')
+#     # modified_row = list(i)
+#     # modified_row[4] = formatDate  # Assuming the date column is the fifth column (index 4)
+
+#     print(i[0], i[1], i[2], i[3], formatDate)
+
+# print("\n")
+
+
+# addStudent = """INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+#                 ('ashley', 'fong', 'ashley.fong@gmail.com', '2021-09-01')"""
+# cursor.execute(addStudent)
+# connection.commit()
+# print("Student added successfully in PostgreSQL ")
+
+# updateStudentEmail = """UPDATE students SET email = 'ashley.fong@example.com' WHERE student_id = 4"""
+# cursor.execute(updateStudentEmail)
+# connection.commit()
+# print("Email updated successfully in PostgreSQl ")
+
+# deleteStudent = """DELETE from students where student_id = 1"""
+# cursor.execute(deleteStudent)
+# connection.commit()
+# print("Student deleted successfully in PostgreSQL ")
